643e467fecb4c27f4b7a310fa3880bc815c04be6
mazfaa <kangazfa@gmail.com>
2022-09-16 19:33:45 +0700

    commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
               </div>
               <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Password</label>
-                <input name="password" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <input name="password" type="password" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
               </div>
               <button type="submit" class="btn btn-primary mt-2"> <i class="bi bi-box-arrow-in-right"></i> Login</button>
             </form>
